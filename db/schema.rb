fcbf100933b695c8bc45a01043f21d597f4d5da8
orikasa <ophi13.k@gmail.com>
2021-06-11 23:17:22 +0900

    Movie モデルと movies テーブルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_11_140340) do
+ActiveRecord::Schema.define(version: 2021_06_11_141048) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "movies", force: :cascade do |t|
+    t.integer "genre", default: 0, null: false
+    t.string "title", null: false
+    t.string "url", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "texts", force: :cascade do |t|
     t.integer "genre", default: 0, null: false
