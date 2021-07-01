bb20e7cc3cb4f5689f427f8b7bb3e92a788d0edb
三浦 <miura.mai910@gmail.com>
2021-07-01 17:08:43 +0900

    テキスト教材のCSVを読み込むコードを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,13 @@
   ActiveRecord::Base.connection.execute("TRUNCATE TABLE #{table_name} RESTART IDENTITY CASCADE")
 end
 
+require "csv"
+
+# テキスト教材のCSVをを読み込み
+CSV.foreach("db/csv_data/text_data.csv", headers: true) do |row|
+  Text.create!(row.to_h)
+end
+
 email = "test@example.com"
 password = "password"
 
