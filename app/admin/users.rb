3f5c0a06b3a39b0434afc020c53941da7f021176
三浦 <miura.mai910@gmail.com>
2021-07-10 19:34:19 +0900

    rubocop -aを実行

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -27,6 +27,15 @@ ActiveAdmin.register User do
     end
     f.actions
   end
+
+  show do
+    attributes_table do
+      row :email
+      row :created_at
+      row :updated_at
+    end
+    active_admin_comments
+  end
   #
   # or
   #
