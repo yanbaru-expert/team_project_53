4d6ea4d3f78b7a95f95c8d9dd5aefcb9dc5680cc
三浦 <miura.mai910@gmail.com>
2021-07-10 15:20:16 +0900

    管理画面からユーザーの新規登録ができるように app/admin/users.rb を変更

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -5,6 +5,28 @@ ActiveAdmin.register User do
   # Uncomment all parameters which should be permitted for assignment
   #
   permit_params :email, :password, :password_confirmation
+
+  index do
+    selectable_column
+    id_column
+    column :email
+    column :created_at
+    column :updated_at
+    actions
+  end
+
+  filter :email
+  filter :created_at
+  filter :updated_at
+
+  form do |f|
+    f.inputs do
+      f.input :email
+      f.input :password
+      f.input :password_confirmation
+    end
+    f.actions
+  end
   #
   # or
   #
