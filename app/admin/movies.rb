4962bbeb2a14d2cd0a485d179cd4bc0cad813fbd
三浦 <miura.mai910@gmail.com>
2021-07-10 15:12:09 +0900

    各ページのカラム、検索、フォームを日本語化し、ジャンルをセレクトボックスで検索できるように変更

--- app/admin/movies.rb
+++ app/admin/movies.rb
@@ -5,6 +5,32 @@ ActiveAdmin.register Movie do
   # Uncomment all parameters which should be permitted for assignment
   #
   permit_params :genre, :title, :url
+
+  index do
+    selectable_column
+    id_column
+    column :genre, :text, &:genre_i18n
+    column :title
+    column :url
+    column :created_at
+    column :updated_at
+    actions
+  end
+
+  filter :genre, as: :select, collection: Text.genres_i18n.invert.transform_values { |v| Text.genres[v] }
+  filter :title
+  filter :url
+  filter :created_at
+  filter :updated_at
+
+  form do |f|
+    f.inputs do
+      f.input :genre, as: :select, collection: Text.genres_i18n.invert
+      f.input :title
+      f.input :url
+    end
+    f.actions
+  end
   #
   # or
   #
--- app/admin/texts.rb
+++ app/admin/texts.rb
@@ -1,11 +1,36 @@
 ActiveAdmin.register Text do
-
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
   # Uncomment all parameters which should be permitted for assignment
   #
   permit_params :genre, :title, :content
+
+  index do
+    selectable_column
+    id_column
+    column :genre, :text, &:genre_i18n
+    column :title
+    column :content
+    column :created_at
+    column :updated_at
+    actions
+  end
+
+  filter :genre, as: :select, collection: Text.genres_i18n.invert.transform_values { |v| Text.genres[v] }
+  filter :title
+  filter :content
+  filter :created_at
+  filter :updated_at
+
+  form do |f|
+    f.inputs do
+      f.input :genre, as: :select, collection: Text.genres_i18n.invert
+      f.input :title
+      f.input :content
+    end
+    f.actions
+  end
   #
   # or
   #
@@ -14,5 +39,4 @@ ActiveAdmin.register Text do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
-  
 end
