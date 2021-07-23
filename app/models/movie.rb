9925e27e485c7867e942a2d90555aa7368c251ee
satoshi07 <satoshi.akaike7@gmail.com>
2021-07-23 18:30:33 +0900

    movieモデルに動画教材の表示場合分けメソッドを定義

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -14,4 +14,13 @@ class Movie < ApplicationRecord
     php: 5
   }
   RAILS_GENRE_LIST = %w[basic git ruby rails].freeze
+  PHP_GENRE_LIST = %w[php].freeze
+
+  def self.genre_select(genre)
+    if genre == php
+      Movie.where(genre: Movie::PHP_GENRE_LIST)
+    else
+      Movie.where(genre: Movie::RAILS_GENRE_LIST)
+    end
+  end
 end
