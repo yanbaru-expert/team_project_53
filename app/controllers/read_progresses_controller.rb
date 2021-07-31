d0ffd34be5a4361d7e198754e60c1de841c06e2f
satoshi07 <satoshi.akaike7@gmail.com>
2021-07-31 17:04:17 +0900

    読破済み機能を非同期処理に変更

--- app/controllers/read_progresses_controller.rb
+++ app/controllers/read_progresses_controller.rb
@@ -1,11 +1,11 @@
 class ReadProgressesController < ApplicationController
   def create
+    @text = Text.find(params[:text_id])
     current_user.read_progresses.create!(text_id: params[:text_id])
-    redirect_back(fallback_location: root_path)
   end
 
   def destroy
+    @text = Text.find(params[:text_id])
     current_user.read_progresses.find_by(text_id: params[:text_id]).destroy!
-    redirect_back(fallback_location: root_path)
   end
 end
