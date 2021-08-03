bf874b08bd4eb4ba870dc3ed87e158b16a250541
orikasa <ophi13.k@gmail.com>
2021-08-04 04:56:22 +0900

    エラーメッセージを修正

--- app/models/watch_progress.rb
+++ app/models/watch_progress.rb
@@ -4,6 +4,6 @@ class WatchProgress < ApplicationRecord
 
   validates :user_id, uniqueness: {
     scope: :movie_id,
-    message: "は同じ動画教材を２回以上読破済みにはできません"
+    message: "は同じ動画教材を２回以上視聴済みにはできません"
   }
 end
